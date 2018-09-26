f173acca480c4cfbfeab8b9ba6243bd2b494a60f
mmorito <morito@mnes.org>
2018-09-26 13:47:58 +0900

    ng g module shared

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ChatComponent } from './chat/chat.component';
+import { ChatComponent } from './chat/components/chat.component';
 
 const routes: Routes = [
   { path: '', component: ChatComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { environment } from '../environments/environment';
 
 import { AngularFireModule } from '@angular/fire';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ChatComponent } from './chat/components/chat.component';
@@ -20,7 +22,11 @@ import { MessageComponent } from './chat/components/message/message.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    SharedModule
+  ],
+  exports: [
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
