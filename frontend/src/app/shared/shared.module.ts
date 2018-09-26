5a9c56e0c7f3e79200a3d024aa67ed83337d8505
mmorito <morito@mnes.org>
2018-09-26 14:09:18 +0900

    ng g component shared/components/auth

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthService } from './services/auth.service';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    AuthService
+    AuthService,
+    AuthComponent
   ]
 })
 export class SharedModule { }
