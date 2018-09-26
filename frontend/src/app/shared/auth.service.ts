7c3356ada95146cc5743b01f9cbd81580bf227b7
mmorito <morito@mnes.org>
2018-09-26 14:05:35 +0900

    add firebase.auth method.

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { }
+
+  public login(): void {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  public logout(): void {
+    this.afAuth.auth.signOut();
+  }
+
 }
